560d73f3be12150753f067a33d0a25ad56614978
иван кузьмич <ivankuzmic210@gmail.com>
2020-09-21 12:25:00 +0300

    42

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ bot.onText(/\/news/, function(msg){
 		if (this.readyState == 4 && this.status == 200) {
 			const html = request.responseText;
 			const $ = ch.load(html);
-			const links = $('a.entry__link').attr('href');
+			const links = $('a.entry__link').attributes('href');
 			console.log(links)
 			// bot.sendMessage(fromId, rofl);
 		}
