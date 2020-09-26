c25eb0e0c8fb9ed412f09ee240392506d12f1124
иван кузьмич <ivankuzmic210@gmail.com>
2020-09-26 23:35:26 +0300

    fix HttpRequest

--- bot.js
+++ bot.js
@@ -1,30 +1,20 @@
 //  Libraries
 const ch = require('cheerio');
 const fs = require('fs');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const request = new XMLHttpRequest();
 
+// node-telegram-bot-api
 const TelegramBot = require('node-telegram-bot-api');
 const Token = process.env.TOKEN;//
 const bot = new TelegramBot(Token, { polling: true });
 
-/*----  help  ----*/
-bot.onText(/\/help/, function(msg){
-	let fromId = msg.from.id;
-	const info = `-----Справочник-----
-				/help - помощь
-				/remind {command} в {time}- напоминалка на определенное время
-				/currency - курс валют
-				/rofl - кидает рофл!10 из 10)))
-				/news - актуальные новости`
-	bot.sendMessage(fromId, info);
-});
-/*----  /help  ----*/
 
 /*----  rofl  ----*/
 bot.onText(/\/rofl/, function(msg){
 	const fromId = msg.from.id;
 	const roflURL = "https://www.anekdot.ru/random/anekdot/";
-	const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-	const request = new XMLHttpRequest();
+
 	request.open('GET', roflURL);
 	request.send();
 
@@ -40,41 +30,23 @@ bot.onText(/\/rofl/, function(msg){
 });
 /*----  /rofl  ----*/
 
-
-/*----  remind  ----*/
-let notes = [];
-
-bot.onText(/\/remind (.+) в (.+)/, function (msg, match) {
-    const userId = msg.from.id;
-    const text = match[1];
-    const time = match[2];
-
-    notes.push({ 'uid': userId, 'time': time, 'text': text });
-
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
-});
-
-setInterval(function(){
-    const timeZone = 3;
-    for (let i = 0; i < notes.length; i++) {
-	    const curDate = new Date().getHours() + timeZone + ':' + new Date().getMinutes();
-	    if (notes[i]['time'] === curDate) {
-	      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
-	      notes.splice(i, 1);
-	    }
-  }
-}, 1000);
-/*----  /remind  ----*/
-
-
 /*----  news  ----*/
+function filterArr(links){
+	let editLinks = [];
+	let i = 0;
+	while(i < Math.floor(links.length / 2))
+	{
+		editLinks.push(links[i]);
+		i += 2;
+	}
+	return editLinks;
+}
+
 bot.onText(/\/news/, function(msg){
 	const fromId = msg.from.id;
-	URLSite = "https://news.tut.by";
+	UrlSite = "https://news.tut.by";
 
-	const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-	const request = new XMLHttpRequest();
-	request.open('GET', URLSite);
+	request.open('GET', UrlSite);
 	request.send();
 
 	request.onreadystatechange = function(){
@@ -83,29 +55,18 @@ bot.onText(/\/news/, function(msg){
 			const $ = ch.load(html);
 			const links = $('a.entry__link').map((i, x) => $(x).attr('href')).toArray();
 
-			function filterArr(links){
-				let editLinks = [];
-				let i = 0;
-				while(i < Math.floor(links.length / 2))
-				{
-					editLinks.push(links[i]);
-					i += 2;
-				}
-				return editLinks;
-			}
 			const lstLinks = filterArr(links);
 
 			let i = 0;
 			const countNews = 10;
 			while(i < countNews)
 			{
-				const query = new XMLHttpRequest();
-				query.open('GET', lstLinks[i]);
-				query.send();
+				request.open('GET', lstLinks[i]);
+				request.send();
 
-				query.onreadystatechange = function() {
+				request.onreadystatechange = function() {
 					if (this.readyState == 4 && this.status == 200) {
-						const html = query.responseText;
+						const html = request.responseText;
 						const $ = ch.load(html);
 						const tag = $('div#article_body').text().replace("\n", " ");
 
@@ -124,9 +85,7 @@ bot.onText(/\/news/, function(msg){
 bot.onText(/\/currency/, function(msg){
 	const fromId = msg.from.id;
 
-	const requestURL = "https://www.nbrb.by/api/exrates/rates?periodicity=0";
-	const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-	const request = new XMLHttpRequest();
+	const currencyURL = "https://www.nbrb.by/api/exrates/rates?periodicity=0";
 	request.open('GET', requestURL);
 
 	request.responseType = 'json';
@@ -154,6 +113,44 @@ bot.onText(/\/currency/, function(msg){
 });
 /*----  /currency  ----*/
 
+/*----  remind  ----*/
+let notes = [];
+
+bot.onText(/\/remind (.+) в (.+)/, function (msg, match) {
+    const userId = msg.from.id;
+    const text = match[1];
+    const time = match[2];
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
+
+setInterval(function(){
+    const timeZone = 3;
+    for (let i = 0; i < notes.length; i++) {
+	    const curDate = new Date().getHours() + timeZone + ':' + new Date().getMinutes();
+	    if (notes[i]['time'] === curDate) {
+	      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
+	      notes.splice(i, 1);
+	    }
+  }
+}, 1000);
+/*----  /remind  ----*/
+
+/*----  help  ----*/
+bot.onText(/\/help/, function(msg){
+	let fromId = msg.from.id;
+	const info = `-----Справочник-----
+				/help - помощь
+				/remind {command} в {time}- напоминалка на определенное время
+				/currency - курс валют
+				/rofl - кидает рофл!10 из 10)))
+				/news - актуальные новости`
+	bot.sendMessage(fromId, info);
+});
+/*----  /help  ----*/
+
 /*----  message  ----*/
 bot.on('message', function (msg) {
 	const chatId = msg.chat.id; // Берем ID чата (не отправителя)
@@ -164,7 +161,7 @@ bot.on('message', function (msg) {
 	  lstImg.push(file);
 	})
 
-	const photo = 'img/' + lstImg[Math.floor(Math.random() * (lstImg.length-1))];
+	const photo = 'img/' + lstImg[Math.floor(Math.random() * (lstImg.length - 1))];
 	bot.sendPhoto(chatId, photo, { caption: 'Лови котейку' });
 
 });
