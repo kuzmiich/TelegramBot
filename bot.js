b3559d965421923d8102012c2eb69c933e380fcb
иван кузьмич <ivankuzmic210@gmail.com>
2020-09-27 21:30:57 +0300

    check

--- bot.js
+++ bot.js
@@ -28,42 +28,17 @@ bot.onText(/\/rofl/, function(msg){
 	request.open('GET', roflURL);
 	request.send();
 
-	let rofl;
+	let rofl = "";
 	request.onreadystatechange = function(){
-		if (this.readyState == 4 && this.status == 200) {
+		if (this.readyState === 4 && this.status === 200) {
 			const html = request.responseText;
 			const $ = ch.load(html);
 			rofl = $('div.text').eq(0).text();
 		}
+		bot.sendMessage(fromId, rofl);
 	}
-	bot.sendMessage(fromId, rofl);
 });
 /*----  /rofl  ----*/
-
-
-/*----  remind  ----*/
-let notes = [];
-bot.onText(/\/remind (.+) в (.+)/, function (msg, match) {
-    const userId = msg.from.id;
-    const text = match[1];
-    const time = match[2];
-
-    notes.push({ 'uid': userId, 'time': time, 'text': text });
-
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
-});
-setInterval(function(){
-    const timeZone = 3;
-    for (let i = 0; i < notes.length; i++) {
-	    const curDate = new Date().getHours() + timeZone + ':' + new Date().getMinutes();
-	    if (notes[i]['time'] === curDate) {
-	      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
-	      notes.splice(i, 1);
-	    }
-  }
-}, 1000);
-/*----  /remind  ----*/
-
 
 /*----  news  ----*/
 function filterArr(links){
@@ -84,9 +59,8 @@ bot.onText(/\/news/, function(msg){
 	request.open('GET', URLSite);
 	request.send();
 
-	let tag;
 	request.onreadystatechange = function(){
-		if (this.readyState == 4 && this.status == 200) {
+		if (this.readyState === 4 && this.status === 200) {
 			const html = request.responseText;
 			const $ = ch.load(html);
 			const links = $('a.entry__link').map((i, x) => $(x).attr('href')).toArray();
@@ -102,10 +76,10 @@ bot.onText(/\/news/, function(msg){
 				query.send();
 
 				query.onreadystatechange = function() {
-					if (this.readyState == 4 && this.status == 200) {
+					if (this.readyState === 4 && this.status === 200) {
 						const html = query.responseText;
 						const $ = ch.load(html);
-						tag = $('div#article_body').text().replace("\n", " ");
+						var tag = $('div#article_body').text().replace("\n", " ");
 					}
 				}
 				i++;
@@ -130,6 +104,7 @@ function currencyList(ArrayObj)
 	}
 	return message;
 }
+
 bot.onText(/\/currency/, function(msg){
 	const fromId = msg.from.id;
 
@@ -140,15 +115,38 @@ bot.onText(/\/currency/, function(msg){
 	request.responseType = 'json';
 	request.send();
 
-	let ArrayObj;
 	request.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
+		let ArrayObj = "";
+		if (this.readyState === 4 && this.status === 200) {
 			ArrayObj = JSON.parse(request.responseText); // get the string from the response
 		}
+		bot.sendMessage(fromId, ArrayObj);
 	}
-	bot.sendMessage(fromId, currencyList(ArrayObj));
 });
 /*----  /currency  ----*/
+
+/*----  remind  ----*/
+let notes = [];
+bot.onText(/\/remind (.+) в (.+)/, function (msg, match) {
+    const userId = msg.from.id;
+    const text = match[1];
+    const time = match[2];
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
+setInterval(function(){
+    const timeZone = 3;
+    for (let i = 0; i < notes.length; i++) {
+	    const curDate = new Date().getHours() + timeZone + ':' + new Date().getMinutes();
+	    if (notes[i]['time'] === curDate) {
+	      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
+	      notes.splice(i, 1);
+	    }
+  }
+}, 1000);
+/*----  /remind  ----*/
 
 /*----  message  ----*/
 bot.on('message', function (msg) {
