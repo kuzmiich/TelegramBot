2bd2e42f9202e6289a75a2a53003c204349000c6
иван кузьмич <ivankuzmic210@gmail.com>
2020-09-27 22:26:33 +0300

    delete async

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const Token = process.env.TOKEN;//
 const bot = new TelegramBot(Token, { polling: true });
 
 /*----  help  ----*/
-bot.onText(/\/help/, async function(msg){
+bot.onText(/\/help/, function(msg){
 	const fromId = msg.from.id;
 	const info = `-----Справочник-----
 				/help - помощь
@@ -16,7 +16,7 @@ bot.onText(/\/help/, async function(msg){
 				/currency - курс валют
 				/rofl - кидает рофл!10 из 10)))
 				/news - актуальные новости`
-	await bot.sendMessage(fromId, info);
+	bot.sendMessage(fromId, info);
 });
 /*----  /help  ----*/
 
@@ -28,13 +28,13 @@ bot.onText(/\/rofl/, function(msg){
 	request.open('GET', roflURL);
 	request.send();
 
-	request.onreadystatechange = async function(){
+	request.onreadystatechange = function(){
 		if (this.readyState === 4 && this.status === 200) {
 			const html = request.responseText;
 			const $ = ch.load(html);
 			const rofl = $('div.text').eq(0).text();
 
-			await bot.sendMessage(fromId, rofl);
+			bot.sendMessage(fromId, rofl);
 		}
 	}
 });
@@ -46,13 +46,13 @@ function parseInfo(fromId, lstLinks, i) {
 	query.open('GET', lstLinks[i]);
 	query.send();
 
-	query.onreadystatechange = async function() {
+	query.onreadystatechange = function() {
 		if (this.readyState === 4 && this.status === 200) {
 			const html = query.responseText;
 			const $ = ch.load(html);
 			const tag = $('div#article_body').text().replace("\n", " ");
 
-			await bot.sendMessage(fromId, tag);
+			bot.sendMessage(fromId, tag);
 		}
 	}
 }
@@ -118,11 +118,11 @@ bot.onText(/\/currency/, function(msg){
 	request.responseType = 'json';
 	request.send();
 
-	request.onreadystatechange = async function() {
+	request.onreadystatechange = function() {
 		if (this.readyState === 4 && this.status === 200) {
 			const ArrayObj = JSON.parse(request.responseText); // get the string from the response
 
-			await bot.sendMessage(fromId, currencyList(ArrayObj));
+			bot.sendMessage(fromId, currencyList(ArrayObj));
 		}
 	}
 });
@@ -132,14 +132,14 @@ bot.onText(/\/currency/, function(msg){
 
 /*----  remind  ----*/
 let notes = [];
-bot.onText(/\/remind (.+) в (.+)/, async function (msg, match) {
+bot.onText(/\/remind (.+) в (.+)/, function (msg, match) {
     const userId = msg.from.id;
     const text = match[1];
     const time = match[2];
 
     notes.push({ 'uid': userId, 'time': time, 'text': text });
 
-    await bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
 });
 setInterval(function(){
     const timeZone = 3;
@@ -154,7 +154,7 @@ setInterval(function(){
 /*----  /remind  ----*/
 
 /*----  message  ----*/
-bot.on('message', async function (msg) {
+bot.on('message', function (msg) {
 	const chatId = msg.chat.id; // Берем ID чата (не отправителя)
 	const path = 'img/';
 	let lstImg = [];
@@ -164,7 +164,7 @@ bot.on('message', async function (msg) {
 	})
 
 	const photo = 'img/' + lstImg[Math.floor(Math.random() * (lstImg.length-1))];
-	await bot.sendPhoto(chatId, photo, { caption: 'Лови котейку' });
+	bot.sendPhoto(chatId, photo, { caption: 'Лови котейку' });
 
 });
 /*----  /message  ----*/
