61573f04c108507a954651494fe891e7a67c8507
Ivan <67104943+kuzmiich@users.noreply.github.com>
2020-10-06 10:57:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -156,13 +156,14 @@ setInterval(function(){
 /*----  message  ----*/
 bot.on('message', function (msg) {
 	const chatId = msg.chat.id; // Берем ID чата (не отправителя)
+	const path = 'img/';
 	let lstImg = [];
 
 	fs.readdirSync(path).forEach(file => {
 	  lstImg.push(file);
 	})
 
-	const photo = 'img/' + lstImg[Math.floor(Math.random() * (lstImg.length-1))];
+	const photo = path + lstImg[Math.floor(Math.random() * (lstImg.length-1))];
 	bot.sendPhoto(chatId, photo, { caption: 'Лови котейку' });
 
 });
